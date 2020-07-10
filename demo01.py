c8a11f25194aef7c779bed4f5e5d2eecdfafd3ce
happy-zhou <15971329035@163.com>
2020-07-10 10:41:31 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,12 @@
+# print("你好，庆毅辉")
+# print(3+4)
+# print("哈哈哈"*20)
+# print(2>100)
+# a = float(input("请输入a值："))
+# b = float(input("请输入b值："))
+# print("输出:",a+b)
+# a='sdfsdfgkvndfjsdmkvlfmvkdflvmskd'
+# print(len(a))
+a=input("请输入字符串1:")
+b=input("请输入字符串2：")
+print(len(a)+len(b))
